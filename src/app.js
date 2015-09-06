6648f2d48867ce70cfd3b4e97c132e7caab4ff7f
Freeman Lou <freemanlou2430@yahoo.com>
2015-09-05 23:45:52 -0400

    Added splash screen and main menu

--- src/app.js
+++ src/app.js
@@ -7,10 +7,58 @@ var ajax = require('ajax');
 var UI = require('ui');
 var Vector2 = require('vector2');
 
+var appendURL = "";
+var apiKey = "accounts?key=4f9385baa549938840c4268f760372f6"; //Time constraint does not allow for more secure methods
+var userAccounts = {};
+var userID = "55e94a6af8d8770528e60e2c/";
+var userName = "Fflewddur Fflam";
+var baseURL = "http://api.reimaginebanking.com/";
 
-var main = new UI.Card({
-  title: 'Capital One',
-  subtitle: ''
+var splashWindow = new UI.Card({
+	title: "Capital One Bank",
+	body: "Retrieving Data..." //Add icons if you have time
 });
 
-main.show();
+splashWindow.show();
+
+//Retrieve Client Info
+var query = baseURL + "customers/" + userID + apiKey; 
+
+ajax(
+	{
+		url: query,
+		type: 'json'
+	},
+	function(data) {
+		userAccounts = data;
+	},
+	function(error) {
+		console.log("Failed to retrieve info");
+	}
+);
+
+// Set up main menu
+var mainMenuItems = [
+	{
+		title: "Accounts"
+	},
+	{
+		title: "Locations",
+		subtitle: "ATMs and Branches"
+	},
+	{
+		title: "My Info",
+		subtitle: "Personal Info"
+	}
+];
+	
+
+var mainMenu = new UI.Menu({
+	sections: [{
+		title: userName,
+		items: mainMenuItems
+	}]
+});
+
+splashWindow.hide();
+mainMenu.show();
