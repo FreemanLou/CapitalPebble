82ebaa396bceefc125b1d04c5cccafcc0b6026f9
Freeman Lou <freemanlou2430@yahoo.com>
2015-09-05 22:37:18 -0400

    Initial Commit

---
+++ src/app.js
@@ -0,0 +1,16 @@
+/**
+ *
+ * Capital One Client on Pebble Watch.
+ */
+
+var ajax = require('ajax');
+var UI = require('ui');
+var Vector2 = require('vector2');
+
+
+var main = new UI.Card({
+  title: 'Capital One',
+  subtitle: ''
+});
+
+main.show();
